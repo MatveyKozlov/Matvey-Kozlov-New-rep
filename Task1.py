2e0dac856ea4f15b74b801d1c66dd790aa352fc6
MatveyKozlov <159297733+MatveyKozlov@users.noreply.github.com>
2025-09-09 10:01:17 +0300

    Add files via upload

---
+++ Task1.py
@@ -0,0 +1,16 @@
+#Представим список в виде переменной:
+
+Season = ["Winter","Spring","Summer","Autumn"]
+
+#Пример кода, создание дека на Python при помощи deque:
+#Метод Deque(Double-Ended Queue) позволяет вставлять и удалять элементы как сначала так и с конца 
+
+from collections import deque
+d = deque() 
+d.append(a)
+d.append(b)
+d.append(c)
+print(d)
+
+
+
