64d08d1e35d34747f4dc037feedc136d3d35327b
MatveyKozlov <159297733+MatveyKozlov@users.noreply.github.com>
2025-09-14 15:37:07 +0300

    Add files via upload

---
+++ task3.java
@@ -0,0 +1,19 @@
+String[] seasons  = new String[4]; // объявили и создали массив на 4 элемента
+
+import java.util.Stack;
+
+public class Geeks 
+{
+    public static void main(String[] args) 
+    {
+        // Создание стека
+        Stack<Integer> s = new Stack<>();
+
+        // Добавление элементов в стек
+        s.push(1);
+        s.push(2);
+        s.push(3);
+        s.push(4);
+    }
+}
+
