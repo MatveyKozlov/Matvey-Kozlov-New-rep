d1aca526d751d9368edd5c39932500309c09e16d
MatveyKozlov <159297733+MatveyKozlov@users.noreply.github.com>
2025-09-14 15:41:28 +0300

    Add files via upload

---
+++ task2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // создание массива длинной 5
+    int arr[5] = {2, 4, 8, 12, 16};
+
+    // создание стека
+
+    // Создание стека целых чисел  
+    std::stack<int> numbers;  
+    // Добавление элементов в стек  
+    numbers.push(1);  
+    numbers.push(2);  
+    numbers.push(3);  
+return 0;
+}
